8ea808b58c3987169e34fd7974c75d3e565217db
maxim necrasov <maddmax22@yandex.ru>
2026-02-19 09:20:43 +0300

    FE: add bopilerplate files

---
+++ vega-fe/src/pages/sign-up-page/slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const signUpSlice = createSlice({
+	name: '@@sign-up',
+	initialState,
+	reducers: {},
+});
+
+export default signUpSlice.reducer;
