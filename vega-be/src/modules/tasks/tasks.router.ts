dbd9fa82f26e44bf0248c7699cec7fa1a3f12616
maxim necrasov <maddmax22@yandex.ru>
2026-02-28 13:41:44 +0300

    FE/BE: add separatly pipelines

--- vega-be/src/modules/tasks/tasks.router.ts
+++ vega-be/src/modules/tasks/tasks.router.ts
@@ -9,4 +9,6 @@ tasksRouter.post(ROUTES.updateTask, updateTask);
 tasksRouter.post(ROUTES.tasks, getUserTasks);
 tasksRouter.get(ROUTES.task, getTaskByUuid);
 
+console.log('test');
+
 export { tasksRouter };
