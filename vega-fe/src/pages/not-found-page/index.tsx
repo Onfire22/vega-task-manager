8ec83260c6d2588d6d85d520444ccc822596c833
maxim necrasov <maddmax22@yandex.ru>
2026-03-21 19:26:35 +0300

    fix 404 page

--- vega-fe/src/pages/not-found-page/index.tsx
+++ vega-fe/src/pages/not-found-page/index.tsx
@@ -1,19 +1,16 @@
 import { Link } from 'react-router-dom';
-import { PageContentWrapper } from '../../components/page-content-wrapper/page-content-wrapper.tsx';
 import './styles.less';
 import { FRONT_ROUTES } from '../../app/constants.ts';
 
 const NotFoundPage = () => {
 	return (
-		<PageContentWrapper>
-			<div className="not-found">
-				<h1 className="not-found__title">404</h1>
-				<div className="not-found__text">Станица не найдена...</div>
-				<Link className="not-found__link" to={FRONT_ROUTES.root}>
-					На главную
-				</Link>
-			</div>
-		</PageContentWrapper>
+		<div className="not-found">
+			<h1 className="not-found__title">404</h1>
+			<div className="not-found__text">Станица не найдена...</div>
+			<Link className="not-found__link" to={FRONT_ROUTES.root}>
+				На главную
+			</Link>
+		</div>
 	);
 };
 
